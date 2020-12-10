7255ac443d865dc95e2184e89e25bef3c9a0b011
Ayush <ayush.yushey@gmail.com>
2020-12-09 22:04:21 -0500

    up

--- express/server.js
+++ express/server.js
@@ -6,7 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-router.get('/threads/3721727/bils-vouches/', (req, res) => {
+router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Expres2s.js!</h1>');
   res.end();
