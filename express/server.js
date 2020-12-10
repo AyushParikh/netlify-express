7c65438c1154449655818fcb7f3ed2c3ec1e4726
Ayush <ayush.yushey@gmail.com>
2020-12-09 22:17:32 -0500

    up

--- express/server.js
+++ express/server.js
@@ -5,31 +5,19 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-// const router = express.Router();
-// router.all('/*', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>Hello from Expres4s.js!</h1>');
-//   res.end();
-// });
-// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-// router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-// app.use(bodyParser.json());
-// app.use('/.netlify/functions/server', router);  // path must route to lambda
-// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
-
-app.get("/foo", function(req,res){
-  res.send('foo');
+const router = express.Router();
+router.all('/*', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
 });
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-//////////////
-var router = express.Router();
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-router.get("/bar", function(req,res,next){
-  res.send('bar');
-});
-
-app.use("/",router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
