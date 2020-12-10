208b130cb040f75a9205a5bc14837da504bcb012
Ayush <ayush.yushey@gmail.com>
2020-12-09 22:46:40 -0500

    up

--- express/server.js
+++ express/server.js
@@ -5,21 +5,25 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.all('/*', (req, res) => {
-  var date = new Date();
-  console.log(date.toString(), ip)
-  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-  res.set('location', 'https://www.sythe.org/threads/3721727/bils-vouches/');
-  res.status(301).send()
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+// const router = express.Router();
+// router.all('/*', (req, res) => {
+//   var date = new Date();
+//   console.log(date.toString(), ip)
+//   var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+//   res.set('location', 'https://www.sythe.org/threads/3721727/bils-vouches/');
+//   res.status(301).send()
+//   res.end();
+// });
+// router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+// router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+// app.use(bodyParser.json());
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.all('*', function(req, res) {
+  res.redirect("https://www.sythe.org/threads/3721727/bils-vouches/");
+});
 
 
 module.exports = app;
