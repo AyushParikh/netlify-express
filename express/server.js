d9a9e9e1f8dde233565d8f009e94c3deeb1964ad
Ayush <ayush.yushey@gmail.com>
2020-12-10 22:04:02 -0500

    update

--- express/server.js
+++ express/server.js
@@ -8,18 +8,19 @@ const fs = require("fs");
 const router = express.Router();
 
 
-app.get('/threads/3721727/bils-vouches/', async function(req, res) {
+app.get('/threads/3721727/bils2-vouches/', async function(req, res) {
+  console.log("here")
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   fs.appendFile('index.html', new Date () + ": " + ip + "<br>", function (err) {
     if (err) throw err;
     console.log('Saved!');
   });
 
-  res.send("tagId is set to " + req.params.tagId);
+  res.send('<html><script>window.location.replace("https://www.sythe.org/threads/3721727/bils-vouches/");</script></html>');
 });
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 
