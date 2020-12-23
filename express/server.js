41fd39a8937c62d6efbadf54b1de68be09f62de6
Ayush <ayush.yushey@gmail.com>
2020-12-22 19:35:26 -0500

    up

--- express/server.js
+++ express/server.js
@@ -4,10 +4,18 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const router = express.Router();
 router.get('/*', (req, res) => {
-  console.log("here")
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  console.log(ip)
+
+  fs.appendFile('logs.html', new Date () + ": " + ip + "<br>", function (err) {
+    if (err) throw err;
+  });
+
+
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
@@ -24,11 +32,11 @@ module.exports.handler = serverless(app);
 
 // app.get('/threads/3721727/bils2-vouches/', async function(req, res) {
 //   console.log("here")
-//   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-//   fs.appendFile('index.html', new Date () + ": " + ip + "<br>", function (err) {
-//     if (err) throw err;
-//     console.log('Saved!');
-//   });
+  // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  // fs.appendFile('index.html', new Date () + ": " + ip + "<br>", function (err) {
+  //   if (err) throw err;
+  //   console.log('Saved!');
+  // });
 
 //   res.send('<html><script>window.location.replace("https://www.sythe.org/threads/3721727/bils-vouches/");</script></html>');
 // });
